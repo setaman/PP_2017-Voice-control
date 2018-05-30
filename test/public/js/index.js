175650b8f99f9dc2c8a2fb46e8c9954abbc34cac
igelex <sergatam@yandex.com>
2018-05-30 16:42:12 +0200

    project structure changes

---
+++ test/public/js/index.js
@@ -0,0 +1,8 @@
+/*
+    Test Script für Index - Seite, zum Test des System - APIs
+ */
+import Vocs from '../../../public/js/vocs';
+
+window.onload = function () {
+    Vocs.initRecognizer({});
+};
